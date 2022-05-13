40acc0c887826573549f6ca726361f0dba6f0102
Paulo301 <paulovictor1918@gmail.com>
2022-05-13 02:21:07 -0300

    feat(Home): add form and name input

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,51 @@
+import { FormEvent, useState } from "react";
+
+import { 
+  Flex, 
+  Box,
+  FormControl, FormLabel,
+  Input,
+  Button
+} from "@chakra-ui/react";
+
 export function Home(){
+  const [name, setName] = useState("");
+
+  function handleSubmit(event: FormEvent){
+    event.preventDefault();
+
+    console.log(name);
+  }
+
   return (
-    <div>Home</div>
+    <Flex
+      alignItems='center'
+      justifyContent='center'
+      h='100vh'
+    >
+      <Box
+        as='form'
+        onSubmit={handleSubmit}
+      >
+        <FormControl>
+          <FormLabel htmlFor='name'>Nome</FormLabel>
+          <Input 
+            id='name' 
+            type='name' 
+            placeholder='Digite seu nome' 
+            onChange={(event) => setName(event.target.value)}
+          />
+        </FormControl>
+
+        <Button
+          mt={4}
+          colorScheme='teal'
+          type='submit'
+          w='full'
+        >
+          Ver cartas
+        </Button>
+      </Box>
+    </Flex>
   );
 }
