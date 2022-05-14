89a6adad130afe51e8bdb8c05ec5abef4a7bcdc6
Paulo301 <paulovictor1918@gmail.com>
2022-05-14 04:51:14 -0300

    feat(Home): add validation for field name

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,27 +1,29 @@
-import { FormEvent, useContext, useEffect, useState } from "react";
+import { FormEvent, useContext, useState } from "react";
 
 import { useNavigate } from "react-router-dom";
+import { SubmitHandler, useForm } from 'react-hook-form';
 
 import { 
   Flex, 
   Box,
-  FormControl, FormLabel,
+  FormControl, FormLabel, FormErrorMessage,
   Input,
   Button
 } from "@chakra-ui/react";
 
 import { NameContext } from "../../hooks/nameStore";
 
+type Inputs = {
+  name: string
+};
+
 export function Home(){
   let navigate = useNavigate();
   const { state, dispatch } = useContext(NameContext);
+  const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm<Inputs>();
 
-  const [name, setName] = useState(state.name);
-
-  function handleSubmit(event: FormEvent){
-    event.preventDefault();
-
-    dispatch({ type: 'SET_NAME', payload: name });
+  const onSubmit: SubmitHandler<Inputs> = (data) =>{
+    dispatch({ type: 'SET_NAME', payload: data.name });
     navigate('/cards');
   }
 
@@ -33,17 +35,26 @@ export function Home(){
     >
       <Box
         as='form'
-        onSubmit={handleSubmit}
+        onSubmit={handleSubmit(onSubmit)}
       >
-        <FormControl>
+        <FormControl isInvalid={!!errors.name}>
           <FormLabel htmlFor='name'>Nome</FormLabel>
           <Input 
             id='name' 
             type='name' 
             placeholder='Digite seu nome' 
-            value={name}
-            onChange={(event) => setName(event.target.value)}
+            defaultValue={state.name}
+            aria-invalid={errors.name ? "true" : "false"}
+            {...register('name', {
+              required: {
+                value: true,
+                message: 'Campo obrigatório'
+              }
+            })}
           />
+          <FormErrorMessage role='alert'>
+            {errors.name && errors.name.message}
+          </FormErrorMessage>
         </FormControl>
 
         <Button
