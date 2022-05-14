f8d3b40490856c194773660656472dcc4eb2d724
Paulo301 <paulovictor1918@gmail.com>
2022-05-14 03:57:08 -0300

    feat(Cards): create basic functioning

--- src/pages/Cards/index.tsx
+++ src/pages/Cards/index.tsx
@@ -1,17 +1,112 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+
+import { GiCardPickup, GiCardExchange } from "react-icons/gi";
+
+import { 
+  Flex,
+  Box, 
+  Heading,
+  SimpleGrid,
+  Button
+} from "@chakra-ui/react";
+
 import { ICard } from "../../interfaces/card";
 import { cardsApi } from "../../services/cardsApi";
+import { NameContext } from "../../hooks/nameStore";
+import { Card } from "../../components/Card";
+import { randomWithRange } from "../../utils/randomWithRange";
+import { shuffle } from "../../utils/shuffle";
 
 export function Cards(){
+  const { state, dispatch } = useContext(NameContext);
+
   const [cards, setCards] = useState<ICard[]>([]);
+  const [drawnCards, setDrawnCards] = useState<ICard[]>([]);
 
   useEffect(() => {
     cardsApi.getCards().then((res) => {
-      setCards(res.data);
+      const tempCards = res.data;
+      const tempDrawnCards = [];
+
+      for(let i=0; i < 5; i++){
+        const index = randomWithRange(0, tempCards.length);
+        tempDrawnCards.push(tempCards.splice(index, 1)[0]);
+      }
+
+      setCards(tempCards);
+      setDrawnCards(tempDrawnCards);
     }).catch((error) => console.log(error))
   }, []);
 
+  function drawCard(){
+    const tempCards = [...cards];
+    const tempDrawnCards = [...drawnCards];
+
+    const index = randomWithRange(0, cards.length);
+    tempDrawnCards.push(tempCards.splice(index, 1)[0]);
+
+    setCards(tempCards);
+    setDrawnCards(tempDrawnCards);
+  }
+
   return (
-    <div>Cards</div>
+    <Flex 
+      flexDir='column' 
+      px='16px'
+    >
+      <Box
+        as='header'
+        display='flex'
+        flexDir='row-reverse'
+      >
+        <Heading 
+          as='h3' 
+          size='lg'
+        >
+          {state.name}
+        </Heading>
+      </Box>
+
+      <SimpleGrid 
+        mt={4}
+        columns={[4]}
+        spacing='20px'
+        justifyItems='center'
+      >
+        {
+          drawnCards.map((card) => (
+            <Card 
+              key={card.id}
+              cardInfo={card} 
+            />
+          ))
+        }
+      </SimpleGrid>
+
+      <SimpleGrid 
+        mt={4}
+        columns={[2]} 
+        spacing='40px' 
+        alignSelf='center'
+      >
+        <Button
+          leftIcon={<GiCardPickup size='18px' />} 
+          colorScheme='teal'
+          isLoading={cards.length === 0}
+          isDisabled={drawnCards.length === 8}
+          onClick={drawCard}
+        >
+          Puxar
+        </Button>
+        <Button
+          leftIcon={<GiCardExchange size='18px' />} 
+          colorScheme='teal'
+          isLoading={cards.length === 0}
+          onClick={() => setDrawnCards(shuffle([...drawnCards]))}
+        >
+          Embaralhar
+        </Button>
+      </SimpleGrid>
+    </Flex>
   );
 } 
